4e00c07bba8de4f61286ba7d4d18a0017c80cc0f
zaru@sakuraba <zarutofu@gmail.com>
2016-04-29 22:15:14 +0900

    change to ajax, and add indicator animation

--- js/techclips.js
+++ js/techclips.js
@@ -3,4 +3,21 @@ $(function() {
     $('#url').val(tab.url);
     $('#title').val(tab.title);
   });
+
+  $('#form').submit(function(event) {
+    event.preventDefault();
+    $('.spinner').show();
+    $('.text').hide();
+    var $form = $(this);
+    $.ajax({
+      url: $form.attr('action'),
+      type: $form.attr('method'),
+      data: $form.serialize(),
+      timeout: 10000,
+      success: function(result, textStatus, xhr) {
+        window.close();
+      },
+      error: function(xhr, textStatus, error) {}
+    });
+  });
 });
